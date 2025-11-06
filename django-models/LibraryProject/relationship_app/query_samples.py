e2e94b7616dfc51895ac211bb487d3efcdacddbf
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-06 22:36:54 +0200

    Minor modifications

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,9 +8,9 @@ author = Author.objects.get(name="J.K. Rowling")
 books_by_author = author.books.all()
 
 # All books in a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name="library_name")
 books_in_library = library.books.all()
 
 # Librarian for a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name="library_name")
 librarian = library.librarian
