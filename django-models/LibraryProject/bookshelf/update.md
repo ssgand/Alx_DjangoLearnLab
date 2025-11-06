461284b76e2e68b3b7da3e6765b94a02291e1959
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-06 22:33:59 +0200

    New folder and file to practice model relationships

---
+++ django-models/LibraryProject/bookshelf/update.md
@@ -0,0 +1,3 @@
+book = Book.objects.get(title='1984')
+book.title='Nineteen Eighty-Four'
+book.save()
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,16 @@
+from .models import Author
+from .models import Library
+from .models import Book
+from .models import Librarian
+
+# All books by a specific author
+author = Author.objects.get(name="J.K. Rowling")
+books_by_author = author.books.all()
+
+# All books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
+
+# Librarian for a library
+library = Library.objects.get(name="Central Library")
+librarian = library.librarian
