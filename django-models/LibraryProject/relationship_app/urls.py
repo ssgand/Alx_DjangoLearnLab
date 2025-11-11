a080e421f354ef23fe5d2be64281cb137c394c79
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-11 21:50:18 +0200

    Minor modifications

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,12 +3,12 @@ from .views import book_list
 from .views import list_books
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
     path("list/", book_list, name="book_list"),
     path("library/<int:pk>/", list_books.as_view(), name="library_detail"),
     path("login/", LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path("logout/", LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path("register/", SignUpView.as_view(), name='register'),
+    path("register/", views.register.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,7 +19,7 @@ class list_books(DetailView):
     template_name = 'relationship_app/library_detail.html'
     # context_object_name = 'book'
 
-class SignUpView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
