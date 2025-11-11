c36f3cd9a5383d0acecaa4fa08c6c56d6177cf5a
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-11 17:10:25 +0200

    Minor modifications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
 from django.views.generic import DetailView, ListView
 
 # Create your views here.
@@ -9,7 +9,7 @@ def book_list(request):
     return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
-    model = Book
+    model = Library
     template_name = 'relationship_app/library_detail.html'
     # context_object_name = 'book'
 
