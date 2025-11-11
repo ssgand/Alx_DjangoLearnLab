2ffad5ed418d487fc33705c9657b39c2e46daaa7
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-11 21:01:03 +0200

    Added new views and template to handle login, logout and signup

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import book_list
 from .views import list_books
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from .views import SignUpView
 
 urlpatterns = [
     path("list/", book_list, name="book_list"),
-    path("library/<int:pk>/", list_books.as_view(), name="library_detail"),  # LibraryDetailView
+    path("library/<int:pk>/", list_books.as_view(), name="library_detail"),
+    path("login/", LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path("logout/", LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path("register/", SignUpView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 def book_list(request):
@@ -13,4 +16,7 @@ class list_books(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     # context_object_name = 'book'
-
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
