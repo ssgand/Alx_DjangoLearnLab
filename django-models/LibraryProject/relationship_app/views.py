3134084cd7e6b424a04ab9022df94cca527dd248
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-11 21:36:03 +0200

    Minor modifications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 # Create your views here.
 def book_list(request):
@@ -16,6 +18,7 @@ class list_books(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     # context_object_name = 'book'
+
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
