8a688883d8f30d3c0e7eb5f4970af74c35dac530
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-11 17:11:27 +0200

    Minor modifications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 # Create your views here.
 def book_list(request):
