ac0513dde75c914531650975bfc71f67bfaa6220
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-14 14:56:46 +0200

    Minor modifications

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
 from django.shortcuts import render
 
 @user_passes_test(lambda user: user.userprofile.role == 'ADMIN')
-def admin_view(request):
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html', {})
