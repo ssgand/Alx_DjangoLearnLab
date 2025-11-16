38ccc96e69d7ca6de8d685599806add31bb59164
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-16 16:35:37 +0200

    Minor modifications

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
-from bookshelf.forms import ExampleForm
+from .forms import ExampleForm
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 
