c29d5ad26d7cf079ab98d080da35b3f103087f15
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-11-22 16:57:33 +0200

    Added authentication to make sure only registered users could use the Book model endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -10,5 +13,7 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
