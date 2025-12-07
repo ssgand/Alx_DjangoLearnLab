0ec4d0d46e1ceb6fd5884a79c80d9fe69be39725
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-12-07 17:25:40 +0200

    Minor modifications

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
