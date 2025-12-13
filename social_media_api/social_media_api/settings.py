e0f3d76fe3149881dda32b0dffd67bd8b0c705ea
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-12-13 23:57:00 +0200

    Started a new app for posts and comments

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts.apps.AccountsConfig',
     'rest_framework.authtoken',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -133,4 +134,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
