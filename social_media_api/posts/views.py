f714ae56bdaf438bcae44172faf3711dec373507
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-12-14 00:49:02 +0200

    Minor modifications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -34,9 +34,7 @@ class FeedView(APIView):
     def get(self, request):
         following_users = request.user.following.all()
 
-        posts = Post.objects.filter(
-            author__in=following_users
-        ).order_by("-created_at")
+        posts = Post.objects.filter(author__in=following_users).order_by("-created_at")
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
