9a0593297170b7b8259b73bf15f6dced057961a3
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-12-14 00:45:59 +0200

    Minor modifications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -32,10 +32,10 @@ class FeedView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        followed_users = request.user.following.all()
+        following_users = request.user.following.all()
 
         posts = Post.objects.filter(
-            author__in=followed_users
+            author__in=following_users
         ).order_by("-created_at")
 
         serializer = PostSerializer(posts, many=True)
